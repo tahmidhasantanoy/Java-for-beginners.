e7aae9fbcd7cb418feb90a890e7b38938beb45fc
Tahmid Hasan <50996889+tahmidhasantanoy@users.noreply.github.com>
2020-02-20 00:31:55 +0600

    Add files via upload

---
+++ Capital_small/Cheaking.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Cheaking {
+	
+	char ch;
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("enter any letter:");
+	ch = input.nextchar();
+	
+	if(x => 'a' && x = 'z' )
+	
+
+}
