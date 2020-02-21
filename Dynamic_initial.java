f0df1d9aaeabaf70ac96cb0d068adec6649b5398
Tahmid Hasan <50996889+tahmidhasantanoy@users.noreply.github.com>
2020-02-21 15:16:26 +0600

    Variable initialization for java.

---
+++ Dynamic_initial.java
@@ -0,0 +1,24 @@
+
+public class Dynamic_initial {
+
+	
+
+		public static void main(String[] args) {
+				
+				boolean b = true;//Dynamic intialization.
+				char c = 'a';
+				short s = 32677;
+				int i;
+				float f = 10.2f;//this data type is little bit ddifference.
+				double d  = 56.89;
+				
+				b = true;
+				c = 'a';
+				System.out.println("b = "+b);
+				System.out.println("c = "+c);
+				System.out.println(s);
+				System.out.println("f = "+f);
+				System.out.println(d);
+			}
+		}
+
