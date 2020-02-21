c4a3859c4c5d38fb01e7ecd6eb567f01bc80e53c
Tahmid Hasan <50996889+tahmidhasantanoy@users.noreply.github.com>
2020-02-21 15:37:53 +0600

    Using the scanner function.

---
+++ Take input by user.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+}public class Scan {
+
+		public static void main() {
+
+			Scanner input = new Scanner(System.in);
+
+			int num;
+			System.out.println("Enter any number");
+			num = input.nextInt();
+
+			System.out.println("Number = "+num);
+		}
+
