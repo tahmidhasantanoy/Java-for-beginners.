6c02fbf7218676ddb3be1d61b416047354e92ca3
Tahmid Hasan <50996889+tahmidhasantanoy@users.noreply.github.com>
2020-02-21 15:42:47 +0600

    This programme is doing for writting comment.

---
+++ CommentDemo.java
@@ -0,0 +1,20 @@
+
+public class Commentdemo {
+	
+ public static void main(String[] args) {
+	 System.out.println("comment learning");//Single line comment
+	 
+	 System.out.println("Multiple line comment");
+	 /*i
+	  * am 
+	  * tahmid
+	  */
+	 System.out.println("documentational comment");
+	 /**
+	  * hiji biji
+	  */
+	 
+	 
+ }
+
+}
